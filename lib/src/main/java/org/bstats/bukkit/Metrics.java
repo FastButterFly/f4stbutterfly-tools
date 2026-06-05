fc8c4e7c43a56dfb832ff609e8494e7a72e702ad
FastButterFly <szymon1.github@gmail.com>
2026-06-05 22:35:13 +0200

    Moved bstats metrics to seperate packages

--- lib/src/main/java/org/bstats/bukkit/Metrics.java
+++ lib/src/main/java/org/bstats/bukkit/Metrics.java
@@ -12,7 +12,7 @@
  *
  * Violations will result in a ban of your plugin and account from bStats.
  */
-package me.f4stbutterfly.tools.bstats;
+package org.bstats.bukkit;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
